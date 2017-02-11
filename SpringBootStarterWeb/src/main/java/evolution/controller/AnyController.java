5b058e7fd7724409ef7b6a7d946d671d35531ad2
user <user@use1r-PC>
2017-02-11 14:09:49 +0800

    new demos

--- SpringBootStarterWeb/src/main/java/evolution/controller/AnyController.java
+++ SpringBootStarterWeb/src/main/java/evolution/controller/AnyController.java
@@ -1,10 +1,20 @@
 package evolution.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import evolution.dto.PostDto;
 import evolution.service.AnotherService;
 import evolution.service.AnyService;
 
@@ -24,5 +34,20 @@ public class AnyController {
 	@RequestMapping(value = "/1", method = RequestMethod.GET)
 	public void runAnotherService() {
 		anotherService.anyMethod();
+	}
+	
+	@GetMapping("/2/{parameter}") 
+	public void getRequest(@PathVariable("parameter") String parameter) {
+		System.out.println(parameter);
+	}
+	
+	@PostMapping("/3")
+	public void postRequest(@RequestBody PostDto postDto) {
+		System.out.println(postDto);
+	}
+	
+	@RequestMapping(value = "/4", method = RequestMethod.GET) 
+	public ResponseEntity<Void> jump() throws URISyntaxException {
+		return ResponseEntity.status(HttpStatus.FOUND).location(new URI("http://www.baidu.com")).build();
 	}
 }
---
+++ SpringBootStarterWeb/src/main/java/evolution/dto/PostDto.java
@@ -0,0 +1,8 @@
+package evolution.dto;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+	private String name;
+}
